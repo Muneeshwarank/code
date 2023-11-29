fc52a2225f8021c82c5c7f803ed4aa351c529595
Muneeshwarank <152406757+Muneeshwarank@users.noreply.github.com>
2023-11-29 19:28:26 +0530

    Add files via upload

---
+++ dllr.ino
@@ -0,0 +1,44 @@
+#include <avr/io.h>     
+#include <util/delay.h> 
+const int led=9; 
+const int temp=A0;
+int temp_val;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+  temp_val=analogRead(temp);
+ int temperature=temp_val*0.4887;
+  Serial.println(temperature);
+  if(temperature>30)
+  {
+  digitalWrite(led,HIGH);
+  Custom_Delay(500);
+  digitalWrite(led,LOW);
+  Custom_Delay(500);
+  }
+  else
+  {
+    digitalWrite(led,HIGH);
+  Custom_Delay(250);
+  digitalWrite(led,LOW);
+  Custom_Delay(250);
+  }
+}
+
+void Custom_Delay(unsigned int ms) 
+{
+    
+    for (unsigned int i = 0; i < ms; i++) 
+    {
+        _delay_ms(1); 
+    }
+}
+ 
+
+ 
+
